9df0ec2d8e6790bfbf35e3db99799a12b8f6e711
Ry4nW <ryanbcwang@gmail.com>
2026-07-10 15:20:39 -0700

    fix(cache): mutex impl

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,34 +2,39 @@ package cache
 
 import (
 	"redis-clone/internal/resp"
+	"sync"
 )
 
-
-// aliasing 
+// aliasing
 type RespValue = resp.RespValue
 
-
 type GenericMapData[K comparable, V any] struct {
-    data map[K]V
+	data map[K]V
 }
 
-
 type Store struct {
+	// shared map
+	mu   sync.RWMutex
 	data map[string]RespValue
 }
 
-func newStore() * Store {
+func newStore() *Store {
 	return &Store{
 		data: make(map[string]RespValue),
 	}
 }
 
-func (store *Store) Get(key string) (RespValue,bool) {
+func (store *Store) Get(key string) (RespValue, bool) {
+	store.mu.RLock()
+	defer store.mu.RUnlock()
+
 	res, ok := store.data[key]
 	return res, ok
 }
 
-func (store *Store) Set(key string, val RespValue)  {
+func (store *Store) Set(key string, val RespValue) {
+	store.mu.RLock()
+	defer store.mu.RUnlock()
+
 	store.data[key] = val
 }
-
