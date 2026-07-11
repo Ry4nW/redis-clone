abaa1065a289d4ede818545ad65613ff4e873e6a
Ry4nW <ryanbcwang@gmail.com>
2026-07-10 18:44:13 -0700

    main handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "redis-clone/internal/server"
+import (
+	"flag"
+	"fmt"
+	"redis-clone/internal/command"
+	"redis-clone/internal/server"
+)
 
 func main() {
-	s := server.New(":8091")
+	port := flag.Int("port", 8091, "port to listen on")
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%d", *port)
+	handlers := command.NewHandlers()
+	s := server.New(addr, handlers)
 	s.Start()
 }
