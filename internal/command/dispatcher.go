a4da5f2d5229c261a85eca8c6a7c3df6e777cc02
Ry4nW <ryanbcwang@gmail.com>
2026-07-10 18:30:09 -0700

    feat: dispatcher and handler methods

--- internal/command/dispatcher.go
+++ internal/command/dispatcher.go
@@ -1,39 +1,151 @@
 package command
 
 import (
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
 	"redis-clone/internal/resp"
 )
 
-func Dispatch(req *resp.Request) (string, error) {
-	switch req.Command {
+type Store map[string]resp.RespValue
+
+type Handlers struct {
+	mu    sync.RWMutex
+	store Store
+}
+
+func NewHandlers() *Handlers {
+	return &Handlers{store: Store{}}
+}
+
+// handling for expiry
+func getNowMS() int64 {
+	return time.Now().UnixMilli()
+}
+
+func (h *Handlers) Dispatch(req *resp.Request) (string, error) {
+	switch strings.ToUpper(req.Command) {
 	case "PING":
-		return HandlePing(req)
+		return h.handlePing(req)
 	case "ECHO":
-		return HandleEcho(req)
+		return h.handleEcho(req)
+	case "GET":
+		return h.handleGet(req)
+	case "SET":
+		return h.handleSet(req)
+	case "EXISTS":
+		return h.handleExists(req)
+	case "DEL":
+		return h.handleDel(req)
+	case "FLUSH":
+		return h.handleFlush(req)
 	default:
 		return "", ErrUnknownCommand
 	}
 }
 
-func HandlePing(req *resp.Request) (string, error) {
-	// 0 arg
-	lenArgs := len(req.Args)
-	if lenArgs == 0 {
+func isValidArgLen(req *resp.Request, expectLen int) bool {
+	return len(req.Args) == expectLen
+}
+
+func (h *Handlers) handlePing(req *resp.Request) (string, error) {
+	if len(req.Args) == 0 {
 		return "PONG\n", nil
 	}
 
-	if lenArgs > 1 {
-		return "", ErrBadArgAmt
-	}
-
-	// 1 arg is echo
-	return HandleEcho(req)
+	return h.handleEcho(req)
 }
 
-func HandleEcho(req *resp.Request) (string, error) {
-	if lenArgs := len(req.Args); lenArgs > 1 || lenArgs == 0 {
+func (h *Handlers) handleEcho(req *resp.Request) (string, error) {
+	if !isValidArgLen(req, 1) {
 		return "", ErrBadArgAmt
 	}
 
 	return req.Args[0] + "\n", nil
+}
+
+func (h *Handlers) handleGet(req *resp.Request) (string, error) {
+	if !isValidArgLen(req, 1) {
+		return "", ErrBadArgAmt
+	}
+
+	key := req.Args[0]
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	entry, ok := h.store[key]
+	if !ok {
+		return "(nil)\n", nil
+	}
+
+	if entry.ExpiresAt != 0 && entry.ExpiresAt <= getNowMS() {
+		delete(h.store, key)
+		return "(nil)\n", nil
+	}
+
+	return entry.String + "\n", nil
+}
+
+func (h *Handlers) handleSet(req *resp.Request) (string, error) {
+	if !isValidArgLen(req, 2) {
+		return "", ErrBadArgAmt
+	}
+
+	key, val := req.Args[0], req.Args[1]
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// TODO: impl PX, EX options
+	h.store[key] = resp.NewBulkString(val)
+
+	return "OK\n", nil
+}
+
+func (h *Handlers) handleExists(req *resp.Request) (string, error) {
+	if !isValidArgLen(req, 1) {
+		return "", ErrBadArgAmt
+	}
+
+	key := req.Args[0]
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	entry, ok := h.store[key]
+	if !ok {
+		return "0\n", nil
+	}
+
+	if entry.ExpiresAt != 0 && entry.ExpiresAt <= getNowMS() {
+		delete(h.store, key)
+		return "0\n", nil
+	}
+
+	return "1\n", nil
+}
+
+func (h *Handlers) handleDel(req *resp.Request) (string, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	deleteCount := 0
+	for _, key := range req.Args {
+		if _, ok := h.store[key]; ok {
+			delete(h.store, key)
+			deleteCount++
+		}
+	}
+	return strconv.Itoa(deleteCount) + "\n", nil
+}
+
+func (h *Handlers) handleFlush(req *resp.Request) (string, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	h.store = Store{}
+	return "OK\n", nil
 }
