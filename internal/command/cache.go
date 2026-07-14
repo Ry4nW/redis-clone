7fc01e032b75b5798c9599d34cc70d102647399b
Ry4nW <ryanbcwang@gmail.com>
2026-07-13 21:09:22 -0700

    fix: temp

--- internal/command/cache.go
+++ internal/command/cache.go
@@ -1,34 +1,31 @@
 package command
 
-import (
-	"redis-clone/internal/resp"
-)
+// import (
+// 	"redis-clone/internal/resp"
+// )
 
+// // aliasing
+// type RespValue = resp.RespValue
 
-// aliasing 
-type RespValue = resp.RespValue
+// type GenericMapData[K comparable, V any] struct {
+// 	data map[K]V
+// }
 
+// type Store struct {
+// 	store map[string]RespValue
+// }
 
-type GenericMapData[K comparable, V any] struct {
-    data map[K]V
-}
+// func newStore() *Store {
+// 	return &Store{
+// 		store: make(map[string]RespValue),
+// 	}
+// }
 
+// func (store *Store) Get(key string) (RespValue, bool) {
+// 	res, ok := store.store[key]
+// 	return res, ok
+// }
 
-type Store struct {
-	data map[string]RespValue
-}
-
-func newStore() * Store {
-	return &Store{
-		data: make(map[string]RespValue),
-	}
-}
-
-func (store *Store) Get(key string) (RespValue,bool) {
-	res, ok := store.data[key]
-	return res, ok
-}
-
-func (store *Store) Set(key string, val RespValue)  {
-	store.data[key] = val
-}
+// func (store *Store) Set(key string, val RespValue) {
+// 	store.store[key] = val
+// }
